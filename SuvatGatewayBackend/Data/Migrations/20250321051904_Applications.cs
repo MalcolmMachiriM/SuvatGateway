f8775ff3f0a6622e1143034655f3391d85b75a72
Malcolm <mmachiri@solugrowth.com>
2025-03-21 07:21:25 +0200

    added the migrations for applications

---
+++ SuvatGatewayBackend/Data/Migrations/20250321051904_Applications.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.DAta.Migrations
+{
+    /// <inheritdoc />
+    public partial class Applications : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Applications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    TransactionFeeChargeType = table.Column<int>(type: "INTEGER", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    PaymentPage = table.Column<bool>(type: "INTEGER", nullable: false),
+                    BusinessId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Applications", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Applications");
+        }
+    }
+}
